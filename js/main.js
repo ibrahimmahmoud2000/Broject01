3660bfdf46f24b7b8367a905d190d44f69710c37
ebrahim mahmoud <genius01278@gmail.com>
2023-06-30 15:22:36 +0300

    hello evryone

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let image = document.querySelector(".img");
+let container = document.querySelector(".container")
+function phones(phone) {
+    image.src = phone;
+}
+function colors(color) {
+    container.style.background = color;
+}
